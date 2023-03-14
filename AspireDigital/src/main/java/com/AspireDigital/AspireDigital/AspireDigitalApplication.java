03c300f5cc7b51cacdcfc16f8577fddcc9d2a9f4
David Mehoves <david.mehoves@aspiredigital.com>
2023-03-13 23:46:04 -0500

    new project

---
+++ AspireDigital/src/main/java/com/AspireDigital/AspireDigital/AspireDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.AspireDigital.AspireDigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspireDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AspireDigitalApplication.class, args);
+	}
+
+}
---
+++ AspireDigital/src/test/java/com/AspireDigital/AspireDigital/AspireDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AspireDigital.AspireDigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AspireDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
