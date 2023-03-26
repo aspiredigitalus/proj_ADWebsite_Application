f259cae4d208a343f54fc8fd08c1c9fd97f9f290
David Mehoves <david.mehoves@aspiredigital.com>
2023-03-26 12:41:06 -0500

    PositionControler

--- AspireDigital/src/main/java/com/AspireDigital/AspireDigital/PositionJoinVal/PositionJoinValRepository.java
+++ AspireDigital/src/main/java/com/AspireDigital/AspireDigital/PositionJoinVal/PositionJoinValRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 @Repository
 public interface PositionJoinValRepository extends JpaRepository<PositionJoinVal, Integer> {
 
-    @Query(value="SELECT p.id, p.employee_id, p.position_id, v.position_title FROM position p JOIN position_val v ON p.position_id = v.position_id WHERE employee_id = :employee_id", nativeQuery = true)
+    @Query(value="SELECT p.id, p.employee_id, p.position_id, v.position_title FROM position p JOIN position_val v ON p.position_id = v.position_id WHERE employee_id = :employee_id ORDER BY v.position_title asc", nativeQuery = true)
     List<PositionJoinVal> findPositionAndName(int employee_id);
 }
