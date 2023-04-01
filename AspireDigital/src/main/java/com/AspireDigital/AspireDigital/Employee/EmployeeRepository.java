2c34b8e3c5f7e9f57cf55c3f9cba52f698b57c9d
David Mehoves <david.mehoves@aspiredigital.com>
2023-03-31 21:03:35 -0500

    added active flag to employee pull

--- AspireDigital/src/main/java/com/AspireDigital/AspireDigital/Employee/EmployeeRepository.java
+++ AspireDigital/src/main/java/com/AspireDigital/AspireDigital/Employee/EmployeeRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 //testing
-    @Query(value="SELECT * FROM employee WHERE region= :region ORDER BY last_name asc", nativeQuery = true)
+    @Query(value="SELECT * FROM employee WHERE region= :region AND active = 1 ORDER BY last_name asc", nativeQuery = true)
     List<Employee> findByRegion(String region);
 
 }
